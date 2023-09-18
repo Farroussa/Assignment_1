5413dbec1edbbc758e243d172b0ce130c8721e2d
Farroussa <104143733+Farroussa@users.noreply.github.com>
2023-09-17 20:38:33 -1000

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+function logMessage() {
+  console.log("Hello, this is my Node.js project!\nand this is my Github Url to the repository :\nhttps://github.com/Farroussa/Assignment_1.git");
+}
+
+logMessage();
